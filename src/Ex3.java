e95fe76b33ae1cd6b3817afbb40638ccfebd7107
MarcellyaAlves <marcelly_figueiredo@hotmail.com>
2023-02-15 16:50:12 -0300

    exercicio java basico

--- src/Ex3.java
+++ src/Ex3.java
@@ -5,12 +5,24 @@ public class Ex3 {
         Scanner scan = new Scanner(System.in);
 
         int numero;
+        int maior = 0;
+        int soma = 0;
 
         int count = 0;
         do {
             System.out.println("Número: ");
             numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if (numero > maior)
+                maior = numero;
+
             count = count + 1;
+
+            System.out.println("Maior: " + maior);
+            System.out.println("Media: " + (soma / 5));
+
         } while (count < 5);
 
     }
